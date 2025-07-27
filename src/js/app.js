ff194ecf644851373a38dae623110871ede78b97
Alejandro Balladares <ale_12_river@hotmail.com>
2025-07-27 17:41:54 -0300

    Agrego galeria con js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-alert('Desde app.js')
+document.addEventListener('DOMContentLoaded', function(){
+    cargarGaleria();
+});
+
+
+
+function cargarGaleria() {
+    const galerias = document.querySelectorAll('.galeria-imagenes');
+    const cantidad_imagenes = 16;
+ 
+    galerias.forEach(galeria => {
+        for (let i = 1; i <= cantidad_imagenes; i++) {
+            const imagen = document.createElement('IMG');
+            imagen.src = `src/img/grande/${i}.jpg`;
+            imagen.alt = `Imagen galeria`;
+ 
+            galeria.appendChild(imagen);
+        }
+    });
+}
